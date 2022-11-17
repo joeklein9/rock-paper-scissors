7ad92c5a62ba3cd4dc40b0ff2fa5ff1802e4dd15
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2022-11-17 18:42:03 -0500

    Update rock-paper-scissors-from-mac.js

--- rock-paper-scissors-from-mac.js
+++ rock-paper-scissors-from-mac.js
@@ -11,6 +11,8 @@ const getComputerChoice = () => {
     }
 }
 
+let userScore = 0
+let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
     
@@ -19,41 +21,58 @@ function playRound(playerSelection, computerSelection) {
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        ++ computerScore
         return 'You lose! Rock beats paper.';
+        ; 
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        ++ userScore;
         return 'You win! Rock beats scissors.';
+        
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        ++ userScore;
         return 'You win! Paper beats rock.';
+        
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        ++ computerScore;
         return 'You lose! Scissors beats paper.';
+        
     }
 
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        ++ computerScore;
         return 'You lose! Rock beats scissors.';
+        
     }
 
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        ++ userScore;
         return 'You win! Scissors beats paper.';
-         
+        
     }
 
 
 }
 
+function displayScore (userScore, computerScore) {
+
+   return 'Your score: ' + userScore + 'Computer score ' + computerScore; 
+}
 
 
 
 function game () {
-    for (let i = 0; i < 5; i++) {
+   for (let i = 0; i < 5; i++) {
       const userInput = prompt("Please input rock, paper, or scissors.");
       alert(playRound (userInput, getComputerChoice()));
-      
+      alert (displayScore(userScore, computerScore));    
     }
 }
+
+
 game();
