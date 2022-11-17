1a3f04d9eee14d98e761f46de17b520e997240a2
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2022-11-17 18:43:29 -0500

    Update rock-paper-scissors-from-mac.js

--- rock-paper-scissors-from-mac.js
+++ rock-paper-scissors-from-mac.js
@@ -14,65 +14,69 @@ const getComputerChoice = () => {
 let userScore = 0
 let computerScore = 0
 
+
 function playRound(playerSelection, computerSelection) {
-    
+
     if (playerSelection === computerSelection) {
         return 'This round is a tie!';
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        ++ computerScore
+        ++computerScore;
         return 'You lose! Rock beats paper.';
-        ; 
+
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        ++ userScore;
+        ++userScore;
         return 'You win! Rock beats scissors.';
-        
+
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        ++ userScore;
+        ++userScore;
         return 'You win! Paper beats rock.';
-        
+
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        ++ computerScore;
+        ++computerScore;
         return 'You lose! Scissors beats paper.';
-        
+
     }
 
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        ++ computerScore;
+        ++computerScore;
         return 'You lose! Rock beats scissors.';
-        
+
     }
 
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        ++ userScore;
+        ++userScore;
         return 'You win! Scissors beats paper.';
-        
+
     }
 
 
 }
 
-function displayScore (userScore, computerScore) {
+function displayScore(userScore, computerScore) {
+    if (userScore > 2) {
+        return 'You win!';
+    }
+    else if (computerScore > 2) {
+        return 'Computer wins!';
+    }
 
-   return 'Your score: ' + userScore + 'Computer score ' + computerScore; 
+    return 'Your score ' + userScore + 'Computer score ' + computerScore;
 }
 
 
-
-function game () {
-   for (let i = 0; i < 5; i++) {
-      const userInput = prompt("Please input rock, paper, or scissors.");
-      alert(playRound (userInput, getComputerChoice()));
-      alert (displayScore(userScore, computerScore));    
+function game() {
+    while (userScore != 3 && computerScore != 3) {
+        const userInput = prompt("Welcome to Rock, Paper, Scissors! You are playing against the computer. First one to 3 points wins! Please input rock, paper, or scissors.");
+        alert(playRound(userInput, getComputerChoice()));
+        alert((displayScore(userScore, computerScore)));
     }
 }
-
-
 game();
